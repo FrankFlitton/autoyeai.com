894d5c9e4b5a6ca8cc0594a87519300a74771972
Frank Flitton <fflitton@gmail.com>
2021-03-06 14:20:59 -0500

    Hash map of documents

--- data/makeAlbums.js
+++ data/makeAlbums.js
@@ -5,6 +5,25 @@ const lyricsData = json.readFile('rawdata/lyrics')
 
 const uniqueAlbums = _.uniq(
   lyricsData.map(song => song.album)
-)
+).sort()
 
-console.log(uniqueAlbums)
+function cleanAlbumName(name) {
+  stringName = typeof name === 'string' ? name : ''
+  return stringName.replace(/ *\([^)]*\) */g, "")
+    .trim()
+}
+
+albumMap = {}
+
+for (let i = 0; i < uniqueAlbums.length; i++) {
+  album = cleanAlbumName(uniqueAlbums[i])
+  albumMap[album] = []
+}
+
+for (let i = 0; i < lyricsData.length; i++) {
+  let title = lyricsData[i].title
+  album = cleanAlbumName(lyricsData[i].album)
+  albumMap[album].push(title)
+}
+
+console.log(albumMap)
