c84ae6fe8c62d287ba27d2d9b421e690c30c2b7f
Frank Flitton <fflitton@gmail.com>
2021-03-06 20:35:02 -0500

    Simplify

--- data/makeAlbums.js
+++ data/makeAlbums.js
@@ -1,41 +1,8 @@
-const json = require('./json')
 const _ = require('lodash')
+const json = require('./json')
+const constants = require('./constants.js')
 
-const lyricsData = json.readFile('rawdata/lyrics')
-
-// Focusing of full releases
-// Collaborations, alternate versions, leaks, live sets...
-// maybe revisit for Yhandi, JIKII, DONDA...
-const nonCanonical = [
-  '',
-  '2016 G.O.O.D. Fridays',
-  'Boys Don’t Cry',
-  'Can’t Tell Me Nothing',
-  'DONDA: WITH CHILD',
-  'Def Poetry Jam',
-  'Freshmen Adjustment Vol. 1',
-  'Freshmen Adjustment Vol. 2',
-  'Freshmen Adjustment Vol. 3',
-  'Get Well Soon...',
-  'I’m Good',
-  'JESUS IS KING II',
-  'Kanye West Presents Good Music Cruel Summer',
-  'Kanye West’s Visionary Streams of Consciousness', // TODO: make a program with just this
-  'Kon the Louis Vuitton Don',
-  'Late Orchestration',
-  'Live at the Paradiso in Amsterdam',
-  'Mike Dean Versions',
-  'Ray Fridays',
-  'The College Dropout: Video Anthology',
-  'The Graduate',
-  'Unreleased Songs',
-  'VH1 Storytellers',
-  'Yandhi'
-]
-
-const uniqueAlbums = _.uniq(
-  lyricsData.map(song => song.album)
-).sort()
+const lyricsDataRaw = json.readFile('rawdata/lyrics')
 
 function cleanAlbumName(name) {
   stringName = typeof name === 'string' ? name : ''
@@ -43,27 +10,33 @@ function cleanAlbumName(name) {
     .trim()
 }
 
+// Setup data
+const lyricsData = lyricsDataRaw.map((song) => {
+  song.album = cleanAlbumName(song.album)
+  song.number = parseInt((song.number || '-1').replace(/[^0-9.]/g, ''))
+  return song
+}).filter((song) => {
+  let isCanon = constants.nonCanonical.indexOf(song.album) === -1
+  let notAltVersion = !(song.title || '').includes('(HW Version)')
+  return isCanon && notAltVersion
+})
+
+
 // Generate hashmap by album name
 albumMap = {}
-for (let i = 0; i < uniqueAlbums.length; i++) {
-  album = cleanAlbumName(uniqueAlbums[i])
-  albumMap[album] = []
-}
-// Sort
 for (let i = 0; i < lyricsData.length; i++) {
-  let title = lyricsData[i].title
-  album = cleanAlbumName(lyricsData[i].album)
-
-  // alternate versions
-  if (!(title || '').includes('(HW Version)')) {
-    albumMap[album].push(`${lyricsData[i].number}. ${title}`)
-    albumMap[album].sort()
+  let song = lyricsData[i]
+  if (!Object.hasOwnProperty.call(albumMap, song.album)) {
+    albumMap[song.album] = []
+  } else {
+    albumMap[song.album].push(song)
   }
 }
-// Remove
-for (const key of nonCanonical) {
-  if (Object.hasOwnProperty.call(albumMap, key)) {
-    delete albumMap[key];
+
+// Sort by track number
+for (const album of Object.keys(albumMap)) {
+  if (Object.hasOwnProperty.call(albumMap, album)) {
+    albumMap[album] = _.sortBy(albumMap[album], ['number'])
   }
 }
 
