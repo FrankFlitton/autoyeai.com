be0b304bd59de958e541b82943adc2d07e2f384d
Frank Flitton <fflitton@gmail.com>
2021-04-15 16:42:40 -0400

    Offsets

--- ui/src/components/grid.js
+++ ui/src/components/grid.js
@@ -2,7 +2,8 @@ import styled from 'styled-components'
 
 // Homebrew flex box auto layout
 
-export const size = { // min width
+// viewport min widths
+export const sizes = {
   xs: '0px',
   sm: '600px',
   md: '960px',
@@ -10,12 +11,16 @@ export const size = { // min width
   xl: '1904px'
 }
 
-const breakpoint = (col, vw) => {
-  if (col) {
-    const colNum = (parseInt(col) / 12 * 100).toFixed(2)
-    const cols = col === 'NaN' ? 'auto' : colNum;
+const getColNum = (col) => {
+  const colNum = (parseInt(col) / 12 * 100).toFixed(2)
+  const isCol = col === 'NaN' ? 'auto' : colNum;
+  return isCol
+}
 
-    return `@media (min-width: ${size[vw]}) {
+const columns = (col, vw) => {
+  if (col) {
+    const cols = getColNum(col)
+    return `@media (min-width: ${vw}) {
         width: ${cols}%;
         min-width: ${cols}%;
       }`
@@ -24,21 +29,35 @@ const breakpoint = (col, vw) => {
   }
 }
 
+const offset = (col, vw) => {
+  if (col) {
+    const cols = getColNum(col)
+    return `@media (min-width: ${vw}) {
+        margin-left: ${cols}%;
+      }`
+  } else {
+    return ''
+  }
+}
+
 const Container = styled.div`
   width: 100%;
-  max-width: ${props => props.fluid ? '100%' : size.md};
+  max-width: ${props => props.fluid ? '100%' : sizes.md};
   padding: 0 1rem !important;
   margin: 0 auto;
+  ${props => props.color ? `background-color: ${props.color};` : ''};
 `;
 
 const Row = styled.div`
   display: flex;
   flex: 1 1 auto;
   flex-wrap: wrap;
+  flex-direction: ${props => props.direction ? props.direction : 'row'};
   align-items: ${props => props.align ? props.align : 'center'};
   justify-content: ${props => props.justify ? props.justify : 'space-between'};
   width: 100%;
   margin: -1rem 0;
+  ${props => props.color ? `background-color: ${props.color};` : ''};
 `;
 
 const Col = styled.div`
@@ -48,11 +67,29 @@ const Col = styled.div`
   max-width: 100%;
   padding: 1rem 0rem;
   width: ${props => props.width ? props.width : 'auto'};
-  ${props => breakpoint(props.xs, 'xs')}
-  ${props => breakpoint(props.sm, 'sm')}
-  ${props => breakpoint(props.md, 'md')}
-  ${props => breakpoint(props.lg, 'lg')}
-  ${props => breakpoint(props.xl, 'xl')}
+
+  // Col size shorthand
+  ${props => props.cols ? columns(props.cols, '0px') : ''}
+  ${props => props.offset ? offset(props.offset, '0px') : ''}
+
+  // Breakpoint compute
+  ${
+    props => {
+      let breakpoints = Object.keys(sizes).map(size => {
+        let css = '';
+        // N col width of 12
+        css += columns(props[size], sizes[size])
+
+        // N col offset of 12
+        const offsetSize = 'offset-' + size
+        css += offset(props[offsetSize], sizes[size])
+        return css
+      })
+      return breakpoints
+    }
+  }
+
+  ${props => props.color ? `background-color: ${props.color};` : ''};
 `;
 
 export { Container, Row, Col }
