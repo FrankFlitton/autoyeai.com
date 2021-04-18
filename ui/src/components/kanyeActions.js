b40564a038236de54e81ec1555eb0b4a9e566d28
Frank Flitton <fflitton@gmail.com>
2021-04-18 17:56:10 -0400

    Efficient spellchecker

--- ui/src/components/kanyeActions.js
+++ ui/src/components/kanyeActions.js
@@ -13,17 +13,27 @@ const KanyeActions = () => {
   const [isFinished, setIsFinished] = useState(false)
 
   function updatePayload (char) {
+    let prevWord = ''
     setLocalPayload((prevState) => {
+      if (char.match(/[ \n]/g)) prevWord = prevState[prevState.length - 1]
       if (char === '\n') {
         return [...prevState, ...[char, '']]
-      } else if (char === ' ') {
-        return [...prevState, ...['']]
+      } else if (char.match(/[ ,?!]/g)) {
+        const next = char.match(/[ ,?!]/g)[0] === ' ' ? '' : char.match(/[ ,?!–—]/g)[0]
+        return [...prevState, ...[next]]
       } else {
         const newState = [...prevState]
         const lastToken = newState.pop()
         return [...newState, ...[lastToken + char]]
       }
     })
+    if (char.match(/[ \n]/g) && prevWord !== '') {
+      setCorrectedText((prevState) => {
+        return char === '\n'
+          ? [...prevState, ...[fixWord(prevWord), '\n']]
+          : [...prevState, ...[fixWord(prevWord)]]
+      })
+    }
   }
 
   const spellcheck = speller
@@ -40,24 +50,32 @@ const KanyeActions = () => {
       } else if (event.data.includes('|')) {
         if (event.data.includes('Text Generation Finished|')) {
           const data = event.data.split('Text Generation Finished|')[1]
-          setIsFinished(true)
           updatePayload(data)
+          setIsFinished(true)
           webWorker.terminate()
           // correctText(data)
         } else if (event.data.includes('Generate Seed|')) {
           const data = event.data.split('Generate Seed|')[1]
+
           if (!!data.length) {
             setSeed(data)
-            const seedTokens = data.split(/[\n ]/gi)
+            let seedTokens = data.split(/[\n ]/g)
+            const startGen = seedTokens.pop()
+            const correctedSeed = seedTokens.map(word => fixWord(word))
+
             setLocalPayload(seedTokens)
+            setCorrectedText(correctedSeed)
+            // setLocalPayload(startGen)
+            updatePayload(' ')
+            updatePayload(startGen)
           }
 
         } else if (event.data.includes('TextData|')) {
           if (!isSpellerTrained) {
             const data = event.data.split('TextData|')[1]
             if (data.length) {
-              spellcheck.train(data)
               setIsSpellerTrained(true)
+              spellcheck.train(data)
             }
           }
         }
@@ -68,58 +86,41 @@ const KanyeActions = () => {
     return;
   }
 
-  const correctText = (tokens) => {
-    if (tokens === undefined) return;
-    if (tokens.length <= 1 ) {
-      setCorrectedText([tokens])
-      return;
+  const fixWord = (word) => {
+    if (word === '\n') return word
+    if (word.match(/[0-9]/)) return word
+    if (word.length <= 1 && word === 'i') {
+      return word.toUpperCase()
     }
 
-    const fixWord = (word) => {
-      if (word === '\n') return word
-      if (word.match(/[0-9]/)) return word
-      if (word.length <= 1 && word === 'i') {
-        return word.toUpperCase()
-      }
+    const isTitleCase = word.charAt(0).match(/[A-Z]/g)
+    const hasPunctuation = word.charAt(word.length - 1).match(/[,?!]/g)
 
-      const isTitleCase = word.charAt(0).match(/[A-Z]/g)
-      const hasPunctuation = word.charAt(word.length - 1).match(/[,?!]/g)
+    let correctedWord = word
 
-      let correctedWord = word
-
-      if (spellcheck.nWords.hasOwnProperty(word.toLowerCase())) {
-        correctedWord = word
-      } else {
-        correctedWord = spellcheck.correct(word)
-        if (hasPunctuation) correctedWord += hasPunctuation[0]
-      }
-
-      if (!censor) {
-        correctedWord = censorWord(correctedWord)
-      }
-      if (!!isTitleCase) {
-        correctedWord = correctedWord.charAt(0).toUpperCase() + correctedWord.slice(1)
-      }
-      return correctedWord
+    if (spellcheck.nWords.hasOwnProperty(word.toLowerCase())) {
+      correctedWord = word
+    } else {
+      correctedWord = spellcheck.correct(word)
+      if (hasPunctuation) correctedWord += hasPunctuation[0]
     }
 
-    const newData = [...tokens]
-    const word = newData.pop()
-    const correctedWord = fixWord(word)
-
-    console.log('tokens:', newData, '\nfix', correctedWord)
-
-    setCorrectedText([...newData, ...[correctedWord]])
-    return;
+    if (!censor) {
+      correctedWord = censorWord(correctedWord)
+    }
+    if (!!isTitleCase) {
+      correctedWord = correctedWord.charAt(0).toUpperCase() + correctedWord.slice(1)
+    }
+    return correctedWord
   }
 
   useEffect(() => {
 
     if (seed === '') runGenerate()
 
-    if (seed !== '' && !isFinished) {
-      correctText(localPayload)
-    }
+    // if (seed !== '' && !isFinished && localPayload.length > correctedText.length) {
+    //   // correctText(localPayload)
+    // }
 
   // eslint-disable-next-line
   }, [seed, localPayload, isFinished])
@@ -148,7 +149,7 @@ const KanyeActions = () => {
       }
       </Col>
       <Col cols={12} sm={6}>
-        payload: { localPayload }
+        payload: <span style={{width: '100%', height: 400}}>{ JSON.stringify(localPayload) }</span>
       </Col>
     </Row>
   )
