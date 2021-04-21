1f652e58f014be1bf7c451d8c46c49cad8c41c6d
Frank Flitton <fflitton@gmail.com>
2021-04-21 09:30:38 -0400

    Fine tune generation settings

--- ui/src/data/generate.js
+++ ui/src/data/generate.js
@@ -4,7 +4,7 @@ export async function generate(inputSeed, textData, model, callback) {
   let seed = inputSeed ? inputSeed : ''
   let onCharGen = callback ? (c) => callback(c) : (char) => console.log(char)
 
-  const temperature = 0.9 // runs a lot faster
+  const temperature = 0.55
   const genLength = 200
 
   return await generateText(
