5f859680257357f84f7da6a41fb474fa91b9d4e9
Frank Flitton <fflitton@gmail.com>
2021-04-21 16:16:30 -0400

    RM kanye graphic placeholder

--- ui/src/components/kanyeSelectorWrapper.js
+++ ui/src/components/kanyeSelectorWrapper.js
@@ -1,56 +1,15 @@
 import { useContext } from 'react'
-import styled from 'styled-components'
+// import styled from 'styled-components'
 import { GeneratorContext } from '../state/generator'
 import { Container, Row, Col } from './grid'
 import KanyeActions from './kanyeActions'
 import KanyeSelector from './kanyeSelector'
-
-const KanyeGraphic = styled.div`
-  min-width: 200px;
-  width: 80%;
-  max-width: 400px;
-  margin: 3rem auto;
-  position: relative;
-  background: red;
-
-  /* Border at bottom to make 3d Effect */
-
-  &:after {
-    content: ' ';
-    width: 100%;
-    height: 10px;
-    background: yellow;
-    position: absolute;
-    bottom: 0;
-    left: 0;
-  }
-  .ratio {
-    padding: 0;
-    padding-top: 100%;
-    background: green;
-    border: 10px solid yellow;
-    box-sizing: border-box;
-  }
-  .graphic {
-    position: absolute;
-    top: 0;
-    left: 0;
-  }
-`
 
 const KanyeSelectorWrapper = () => {
   const { dataSet } = useContext(GeneratorContext)
 
   return (
     <Container id="generate">
-      <Row>
-        <Col width="100%">
-          <KanyeGraphic>
-            <div className="ratio"></div>
-            <div className="graphic"> ActiveKanye: {dataSet.title} </div>
-          </KanyeGraphic>
-        </Col>
-      </Row>
       <KanyeSelector />
       <KanyeActions />
     </Container>
